36edf2bef1efcaf85783fdc72481e04ae8a1423b
lucastigrereal-dev <i9lucastigre@gmail.com>
2026-09-26 15:14:07 -0300

    feat: register monetary adjustment router

--- server/routers.ts
+++ server/routers.ts
@@ -24,6 +24,7 @@ import { projectSettingsRouter } from "./routers/projectSettings";
 import { commercialPoliciesRouter } from "./routers/commercialPolicies";
 import { intelligenceRouter } from "./routers/intelligence";
 import { inventoryRouter } from "./routers/inventory";
+import { monetaryAdjustmentsRouter } from "./routers/monetaryAdjustments";
 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
@@ -83,6 +84,7 @@ export const appRouter = router({
   commercialPolicies: commercialPoliciesRouter,
   intelligence: intelligenceRouter,
   inventory: inventoryRouter,
+  monetaryAdjustments: monetaryAdjustmentsRouter,
 });
 
 export type AppRouter = typeof appRouter;
